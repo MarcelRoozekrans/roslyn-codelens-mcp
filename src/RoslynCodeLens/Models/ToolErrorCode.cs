1066650ccc5846f75a2ab54dce87beb1ccc9b7c2
Marcel Roozerkans <marcel@roozekrans.nl>
2026-05-26 08:31:26 +0200

    feat(errors): add McpToolException + ToolErrorCode enum

---
+++ src/RoslynCodeLens/Models/ToolErrorCode.cs
@@ -0,0 +1,12 @@
+namespace RoslynCodeLens.Models;
+
+public enum ToolErrorCode
+{
+    SymbolNotFound,
+    SolutionNotTrusted,
+    AmbiguousMatch,
+    FileNotFound,
+    ProjectNotFound,
+    InvalidArgument,
+    Internal,
+}
